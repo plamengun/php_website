0b2917fdd78c7f2b16d1a4f637c95004c9f7ca0f
Plamen Gunchev <plamenmgunchev@gmail.com>
2024-10-27 18:23:47 +0200

    Connection plus inital test file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    // echo "I love pizza <br>"; 
+    // echo "It's really good </br>";
+    // //This is a comment
+    // $password = "pizza123";
+    // $hash = password_hash($password, PASSWORD_DEFAULT);
+    // echo "$hash </br>";
+
+    // if (password_verify("pizza123", $hash)){
+    //     echo "You are logged in!";
+    // }
+    // else{
+    //     echo "Incorrect password";
+    // }
+?>
+<!DOCTYPE html>
